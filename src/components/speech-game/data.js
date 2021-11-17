d7e87b56ebf10bba95dcab771a0d3bcada45cbcb
amrita154 <guptaamrita154@gmail.com>
2021-11-17 13:59:11 +0530

    speech game

---
+++ src/components/speech-game/data.js
@@ -0,0 +1,23 @@
+const gameData=[
+    {
+        id:1,
+        object:"Bird",
+        action:"fly",
+        animation:require('./../../assets/animation/flappy-bird-delivering-a-message.json'),
+
+    },
+    {
+        id:2,
+        object:"Tiger",
+        action:"move",
+        animation:require('./../../assets/animation/lion-running.json'),
+    },{
+         id:3,
+        object:"Rain",
+        action:"start rainfall",
+        animation:require('./../../assets/animation/rain-animation-with-umbrella.json'),
+    }
+]
+
+
+export default gameData;
---
+++ src/components/speech-game/index.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef, useState } from "react";
+import lottie from "lottie-web";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import gameData from './data';
+
+const SpeechToText = () => {
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const containerRef = useRef(null);
+  const [id,setId]=useState(0);
+
+  const onPressNext=()=>{
+      if(id<2){
+          setId(id+1);
+      }
+      else{
+          setId(0);
+      }
+      resetTranscript();
+      lottie.destroy("animation");
+  }
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: containerRef.current,
+      animationData: gameData[id].animation,
+      loop:true,
+      autoplay:true,
+      name:"animation"
+    });
+      return () => lottie.stop();
+  },[id]);
+
+  useEffect(() => {
+      console.log("transcript",transcript);
+      let temp=[];
+     temp=transcript.split(' ');
+    if (temp[temp.length-1]===gameData[id].object.toLowerCase()||temp[temp.length-1]===gameData[id].object) {
+      lottie.play("animation");
+    } else {
+      lottie.pause("animation");
+      resetTranscript();
+    }
+  }, [transcript,id]);
+
+  useEffect(() => {
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+      alert("Browser does not support speech recognition");
+    }
+    SpeechRecognition.startListening({ continuous: true });
+    console.log("Now listening...");
+    return () => {
+      SpeechRecognition.stopListening();
+      console.log("Stopped Listening");
+    };
+  }, []);
+
+
+  return (
+    <div>
+      <h1> Speak {gameData[id].object} to {gameData[id].action}</h1>
+      <h3>{transcript}</h3>
+      <div id="animation" ref={containerRef} style={{height:'40%',width:'40%'}}/>
+      <button onClick={onPressNext}>Next</button>
+    </div>
+  );
+};
+
+export default SpeechToText;
